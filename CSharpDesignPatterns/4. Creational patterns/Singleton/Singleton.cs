00df73cfbef5e492e32bb2bbfbcefa8a33743675
Marcin Konrad Ceglarek <marcin.ceglarek@redington.co.uk>
2019-06-11 01:15:49 +0200

    Added Singleton excercise

---
+++ CSharpDesignPatterns/4. Creational patterns/Singleton/Singleton.cs
@@ -0,0 +1,19 @@
+﻿namespace CSharpDesignPatterns._4._Creational_patterns.Singleton
+{
+    using System;
+
+    internal class Singleton
+    {
+        private Singleton()
+        {
+            this.Id = Guid.NewGuid();
+        }
+
+        public static Singleton GetInstance()
+        {
+            return null;
+        }
+
+        public Guid Id { get; }
+    }
+}
---
+++ CSharpDesignPatterns/4. Creational patterns/Singleton/SingletonTests.cs
@@ -0,0 +1,31 @@
+﻿namespace CSharpDesignPatterns._4._Creational_patterns.Singleton
+{
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class SingletonTests
+    {
+        [Test]
+        [Repeat(25)]
+        public async Task Singleton_AccessFromMultipleTasks_SameGuid()
+        {
+            var tasks = Enumerable.Range(0, 5).AsParallel().Select(t => Task.Run(() => Singleton.GetInstance()));
+            var singletons = await Task.WhenAll(tasks);
+
+            var firstGuid = singletons[0].Id;
+            Assert.IsTrue(singletons.All(s => s.Id == firstGuid));
+        }
+
+        [Test]
+        public void SingletonTests_GetInstanceTwice_SameGuid()
+        {
+            var instance1 = Singleton.GetInstance();
+            var instance2 = Singleton.GetInstance();
+
+            Assert.AreEqual(instance1.Id, instance2.Id);
+        }
+    }
+}
