9c8ee3cc3b40f9e54bfe4dd30ae79ff69e6d704d
Marcin Konrad Ceglarek <marcin.ceglarek@redington.co.uk>
2019-06-11 10:21:24 +0100

    Added MarketTests

---
+++ CSharpDesignPatterns/5. Structural patterns/Marker/Marker.cs
@@ -0,0 +1,53 @@
+﻿namespace CSharpDesignPatterns._5._Structural_patterns.Marker
+{
+    using System;
+
+    public interface IAlive
+    {
+    }
+
+    public class PlantAttribute : Attribute
+    {
+    }
+
+    public class AnimalAttribute : Attribute
+    {
+    }
+
+    public class SentientAttribute : Attribute
+    {
+    }
+
+    public abstract class Plant : IAlive
+    {
+    }
+
+    public class Tree : Plant
+    {
+    }
+
+    public class Mushroom : Plant
+    {
+    }
+
+    public class Bush : Plant
+    {
+    }
+
+    public class Animal : IAlive
+    {
+    }
+
+    public class Dog : Animal
+    {
+    }
+
+    public class Elephant : Animal
+    {
+    }
+
+    public class Human : Animal
+    {
+
+    }
+}
---
+++ CSharpDesignPatterns/5. Structural patterns/Marker/MarkerTests.cs
@@ -0,0 +1,59 @@
+﻿namespace CSharpDesignPatterns._5._Structural_patterns.Marker
+{
+    using System.Collections.Generic;
+    using System.Linq;
+
+    using Castle.Core.Internal;
+
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class MarkerTests
+    {
+        private readonly IList<IAlive> aliveEntities = new List<IAlive>()
+                                                  {
+                                                      new Human(),
+                                                      new Elephant(),
+                                                      new Human(),
+                                                      new Dog(),
+                                                      new Mushroom(),
+                                                      new Mushroom(),
+                                                      new Tree(),
+                                                      new Bush()
+                                                  };
+        [Test]
+        public void SentientEntities()
+        {
+            var sentientItems = this.aliveEntities.Where(i => i.GetType().HasAttribute<SentientAttribute>()).ToList();
+
+            Assert.AreEqual(2, sentientItems.Count);
+            CollectionAssert.AllItemsAreInstancesOfType(sentientItems, typeof(Human));
+        }
+
+        [Test]
+        public void PlantEntities()
+        {
+            var sentientItems = this.aliveEntities.Where(i => i.GetType().HasAttribute<PlantAttribute>()).ToList();
+
+            Assert.AreEqual(4, sentientItems.Count);
+        }
+
+        [Test]
+        public void AnimalEntities()
+        {
+            var sentientItems = this.aliveEntities.Where(i => i.GetType().HasAttribute<AnimalAttribute>()).ToList();
+
+            Assert.AreEqual(4, sentientItems.Count);
+        }
+
+        [Test]
+        public void NotSentientAnimalsEntities()
+        {
+            var sentientItems = this.aliveEntities.Where(
+                i => i.GetType().HasAttribute<AnimalAttribute>()
+                     && !i.GetType().HasAttribute<SentientAttribute>()).ToList();
+
+            Assert.AreEqual(2, sentientItems.Count);
+        }
+    }
+}
