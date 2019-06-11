ab57cbff0e9c5cd17f6f7e723fcb482a8a3a5947
Marcin Konrad Ceglarek <marcin.ceglarek@redington.co.uk>
2019-06-11 15:00:12 +0100

    Hiding Flyweight implementation

--- CSharpDesignPatterns/5. Structural patterns/Flyweight/Flyweight.cs
+++ CSharpDesignPatterns/5. Structural patterns/Flyweight/Flyweight.cs
@@ -2,37 +2,21 @@
 {
     using System;
     using System.Collections.Generic;
-    using System.Linq;
 
     class FlyweightCoffeeShop
     {
-        public void TakeOrder(Guid customerId, CoffeeFlavour flavour)
-        {
-            if (this.Orders.ContainsKey(customerId))
-            {
-                return;
-            }
-
-            if (!this.CoffeeFlavours.ContainsKey(flavour.Flavour))
-            {
-                this.CoffeeFlavours.Add(flavour.Flavour, flavour);
-            }
-
-            this.Orders.Add(customerId, this.CoffeeFlavours[flavour.Flavour]);
-        }
-
         public IDictionary<string, CoffeeFlavour> CoffeeFlavours { get; } = new Dictionary<string, CoffeeFlavour>();
 
         public IDictionary<Guid, CoffeeFlavour> Orders { get; } = new Dictionary<Guid, CoffeeFlavour>();
 
+        public void TakeOrder(Guid customerId, CoffeeFlavour flavour)
+        {
+            throw new NotImplementedException();
+        }
+
         public void TakeOrder(Guid customerId, string flavour)
         {
-            if (!this.CoffeeFlavours.ContainsKey(flavour))
-            {
-                this.CoffeeFlavours.Add(flavour, new CoffeeFlavour(flavour));
-            }
-
-            this.Orders.Add(customerId, this.CoffeeFlavours[flavour]);
+            throw new NotImplementedException();
         }
     }
 
