2f68eb6359098dd11a13974596d53eff32499f09
Marcin Konrad Ceglarek <marcin.ceglarek@redington.co.uk>
2019-06-12 02:19:11 +0200

    Added Strategy

---
+++ CSharpDesignPatterns/6. Behavioral patterns/Strategy/Strategy.cs
@@ -0,0 +1,50 @@
+﻿namespace CSharpDesignPatterns._6._Behavioral_patterns.Strategy
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class Customer
+    {
+        private readonly IList<double> drinksPrices;
+
+        public Customer(IBillingStrategy strategy)
+        {
+            this.drinksPrices = new List<double>();
+            this.Strategy     = strategy;
+        }
+
+        public IBillingStrategy Strategy { get; set; }
+
+        public void Add(double price, int quantity)
+        {
+            this.drinksPrices.Add(this.Strategy.GetPrice(price * quantity));
+        }
+
+        public double GetTotalAmount()
+        {
+            return this.drinksPrices.Sum();
+        }
+    }
+
+    public interface IBillingStrategy
+    {
+        double GetPrice(double originalPrice);
+    }
+
+    public class NormalStrategy : IBillingStrategy
+    {
+        public double GetPrice(double originalPrice)
+        {
+            return originalPrice;
+        }
+    }
+
+    public class HappyHourStrategy : IBillingStrategy
+    {
+        public double GetPrice(double originalPrice)
+        {
+            return originalPrice * 0.5;
+        }
+    }
+}
---
+++ CSharpDesignPatterns/6. Behavioral patterns/Strategy/StrategyTests.cs
@@ -0,0 +1,53 @@
+﻿namespace CSharpDesignPatterns._6._Behavioral_patterns.Strategy
+{
+    using NUnit.Framework;
+    using NUnit.Framework.Internal;
+
+    [TestFixture]
+    public class StrategyTests
+    {
+        private IBillingStrategy normalStrategy = new NormalStrategy();
+        private IBillingStrategy happyHourStrategy = new HappyHourStrategy();
+
+        [Test]
+        public void Customer_UsingNormalStrategy_PaysExpectedAmount()
+        {
+            var customer = new Customer(this.normalStrategy);
+
+            customer.Add(5, 5);
+            customer.Add(1, 5);
+            customer.Add(3, 5);
+
+            Assert.AreEqual(25 + 5 + 15, customer.GetTotalAmount());
+        }
+
+
+        [Test]
+        public void Customer_UsingHappyHourStrategy_PaysExpectedAmount()
+        {
+            var customer = new Customer(this.happyHourStrategy);
+
+            customer.Add(5, 5);
+            customer.Add(1, 5);
+            customer.Add(3, 5);
+
+            Assert.AreEqual((25.0 + 5 + 15) / 2, customer.GetTotalAmount());
+        }
+
+        [Test]
+        public void Customer_SwitchingStrategies_PaysExpectedAmount()
+        {
+            var customer = new Customer(this.happyHourStrategy);
+
+            customer.Add(5, 5);
+            customer.Add(1, 5);
+
+            customer.Strategy = this.normalStrategy;
+
+            customer.Add(3, 5);
+            customer.Add(2, 5);
+
+            Assert.AreEqual(25.0 / 2 + 2.5 + 15 + 10, customer.GetTotalAmount());
+        }
+    }
+}
