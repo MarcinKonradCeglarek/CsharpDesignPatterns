f0f55dec60992c4a5715eedf2f0838bce5f1b8ab
Marcin Konrad Ceglarek <marcin.ceglarek@redington.co.uk>
2019-06-12 07:59:06 +0100

    Revert "Hiding Flyweight implementation"
    This reverts commit ab57cbff0e9c5cd17f6f7e723fcb482a8a3a5947.

--- CSharpDesignPatterns/5. Structural patterns/Flyweight/Flyweight.cs
+++ CSharpDesignPatterns/5. Structural patterns/Flyweight/Flyweight.cs
@@ -2,20 +2,36 @@
 {
     using System;
     using System.Collections.Generic;
+    using System.Linq;
 
     internal class FlyweightCoffeeShop
     {
+        public void TakeOrder(Guid customerId, CoffeeFlavour flavour)
+        {
+            if (this.Orders.ContainsKey(customerId))
+            {
+                return;
+            }
+
+            if (!this.CoffeeFlavours.ContainsKey(flavour.Flavour))
+            {
+                this.CoffeeFlavours.Add(flavour.Flavour, flavour);
+            }
+
+            this.Orders.Add(customerId, this.CoffeeFlavours[flavour.Flavour]);
+        }
+
         public IDictionary<string, CoffeeFlavour> CoffeeFlavours { get; } = new Dictionary<string, CoffeeFlavour>();
         public IDictionary<Guid, CoffeeFlavour> Orders         { get; } = new Dictionary<Guid, CoffeeFlavour>();
 
         public void TakeOrder(Guid customerId, CoffeeFlavour flavour)
         {
-            throw new NotImplementedException();
-        }
+            if (!this.CoffeeFlavours.ContainsKey(flavour))
+            {
+                this.CoffeeFlavours.Add(flavour, new CoffeeFlavour(flavour));
+            }
 
-        public void TakeOrder(Guid customerId, string flavour)
-        {
-            throw new NotImplementedException();
+            this.Orders.Add(customerId, this.CoffeeFlavours[flavour]);
         }
     }
 
