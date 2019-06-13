d5651221f57476a7b64a4f937b6ac6f0d61164fd
Marcin Konrad Ceglarek <marcin.ceglarek@redington.co.uk>
2019-06-13 08:23:58 +0100

    Added Specification

---
+++ CSharpDesignPatterns/6. Behavioral patterns/Specification/SpecificationTests.cs
@@ -0,0 +1,33 @@
+﻿namespace CSharpDesignPatterns._6._Behavioral_patterns.Specification
+{
+    using System;
+
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class SpecificationTests
+    {
+        [Test]
+        public void Specification_OrAnd_ProperlyWorksForGivenConditions()
+        {
+            var greaterThan0 = new IntGreaterThan(0);
+            var lessThan10 = new IntLessThan(10);
+
+            var over9000 = new IntGreaterThan(9000);
+        
+            var myCondition = over9000.Or(greaterThan0.And(lessThan10));
+
+            Assert.IsTrue(myCondition.IsSatisfiedBy(5));
+            Assert.IsTrue(myCondition.IsSatisfiedBy(9999));
+        }
+
+        [Test]
+        public void X()
+        {
+            Func<int, bool> myCondition = a => a > 9000 | (a > 0 && a < 10);
+
+            Assert.IsTrue(myCondition(5));
+            Assert.IsTrue(myCondition(9999));
+        }
+    }
+}
