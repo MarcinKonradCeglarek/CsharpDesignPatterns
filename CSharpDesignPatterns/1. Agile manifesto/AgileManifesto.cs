b4c90204c53181cba1a7fb30fdb175769b247433
Marcin Konrad Ceglarek <marcin.ceglarek@redington.co.uk>
2019-06-27 08:20:57 +0100

    Agile manifesto

--- CSharpDesignPatterns/1. Agile manifesto/AgileManifesto.cs
+++ CSharpDesignPatterns/1. Agile manifesto/AgileManifesto.cs
@@ -4,6 +4,17 @@
 
     internal class AgileManifesto
     {
-        public static Url ManifestoUrl { get; set; } = new Url("https://agilemanifesto.org");
+        /*
+         * 11-13.02.2001 Snowbird, Utah
+         *
+         * Individuals and interactions     over processes and tools
+         * Working software                 over comprehensive documentation
+         * Customer collaboration           over contract negotiation
+         * Responding to change             over following a plan
+         */
+
+        public static Url ManifestoUrl { get; } = new Url("https://agilemanifesto.org");
+
+        public static Url WikiPage { get; } = new Url("https://pl.wikipedia.org/wiki/Manifest_Agile");
     }
 }
--- CSharpDesignPatterns/2. Design principles/CleanCode/CleanCode.cs
+++ CSharpDesignPatterns/2. Design principles/CleanCode/CleanCode.cs
@@ -6,14 +6,12 @@ namespace CSharpDesignPatterns._2._Design_principles.CleanCode
     {
         private static Url OReillyUrl { get; } = new Url("https://www.oreilly.com/library/view/clean-code/9780136083238/");
 
-        private static string[] UsefulTools { get; } = { "StyleCop", "ESLint" };
+        private static string[] UsefulTools { get; } = { "Resharper.StyleCop", "ESLint" };
 
         public static int AlwaysUseBraces(int i)
         {
-            for (var j = 0; j < 100; j++)
-            {
+            for (var j = 0; j < 100; j++);
                 i++;
-            }
 
             // What will be return value?
             return i;
