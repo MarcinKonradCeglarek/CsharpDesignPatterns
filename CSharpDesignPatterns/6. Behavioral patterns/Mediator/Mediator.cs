d87f5ad6b7f3e676b87e21bfd172896a4da3fe23
Marcin Konrad Ceglarek <marcin.ceglarek@redington.co.uk>
2019-06-28 08:54:29 +0100

    Added comments for mediator

--- CSharpDesignPatterns/6. Behavioral patterns/Mediator/Mediator.cs
+++ CSharpDesignPatterns/6. Behavioral patterns/Mediator/Mediator.cs
@@ -1,4 +1,4 @@
-﻿namespace CSharpDesignPatterns._6._Behavioral_patterns.Mediator
+namespace CSharpDesignPatterns._6._Behavioral_patterns.Mediator
 {
     using System;
     using System.Collections.Generic;
@@ -23,6 +23,9 @@
         public ChatClient(string name, ChatRoomMediator mediator, IReceivedMessagesHandler receivedMessagesHandler)
         {
             throw new NotImplementedException();
+            /*
+             * Register with mediator (so that mediator knows who dispatch messages to)
+             */
         }
 
         public string Name { get; }
@@ -43,6 +46,9 @@
         public ChatMessageCounter(ChatRoomMediator mediator)
         {
             throw new NotImplementedException();
+            /*
+             * Register with mediator (so that mediator knows who dispatch messages to)
+             */
         }
 
         public int Counter { get; set; }
@@ -63,10 +69,16 @@
     public class ChatRoomMediator
     {
         public ICollection<IChatRoomClient> Clients { get; } = new List<IChatRoomClient>();
+            /*
+             * Register client and it's callback function (used when sending him message)
+             */
 
         public void SendMessage(IChatRoomClient client, string message)
         {
-            throw new NotImplementedException();
+            /*
+             * Check if client is on mediator's clients list
+             *   if yes, broadcast messages to all other clients
+             */
         }
     }
 }
