955dfb7d7e5fe5c537a993488e0079cd450324c4
Marcin Konrad Ceglarek <marcin.ceglarek@redington.co.uk>
2019-12-03 12:04:22 +0000

    Jaks zmiana

--- CSharpDesignPatterns/5. Structural patterns/Flyweight/TreeRepositoryFlyweight.cs
+++ CSharpDesignPatterns/5. Structural patterns/Flyweight/TreeRepositoryFlyweight.cs
@@ -7,17 +7,25 @@ namespace CSharpDesignPatterns._5._Structural_patterns.Flyweight
 
     internal class TreeRepositoryFlyweight
     {
+        private int counter = 0;
         public IDictionary<int, Tree> Trees      { get; } = new Dictionary<int, Tree>();
         public IDictionary<string, TreeModel> TreeModels { get; } = new Dictionary<string, TreeModel>();
 
         public int CreateTree(TreeModel treeModel, Position position)
         {
-            throw new NotImplementedException();
+            return this.CreateTree(treeModel.Name, position);
         }
 
         public int CreateTree(string name, Position position)
         {
-            throw new NotImplementedException();
+            var treeModel = new TreeModel(name);
+            if (!this.TreeModels.ContainsKey(name))
+            {
+                this.TreeModels.Add(treeModel.Name, treeModel);
+            }
+
+            this.Trees.Add(counter, new Tree(treeModel, position));
+            return counter++;
         }
     }
 }
